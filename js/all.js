943f0e8adbc50c23494bc2892d636bd5822a7e93
ting0812 <61533369+ting0812@users.noreply.github.com>
2022-01-27 15:41:33 +0800

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,79 @@
+const keyword = document.querySelector(".keyword");
+const list = document.querySelector(".list");
+const btn = document.querySelector(".btn");
+
+function init(data){  
+    let str = "";
+  
+    data.forEach(function(item){
+      str += `<li>${item[2]}</li>`;
+    })
+    list.innerHTML = str;
+}
+
+function getWord1475() {
+    return axios.get('https://a1073315.github.io/Information-Retrieval/txt/word1475.txt');
+}
+function getWeight1() {
+    return axios.get('https://a1073315.github.io/Information-Retrieval/txt/weight1.txt');
+}
+function getFilename() {
+    return axios.get('https://a1073315.github.io/Information-Retrieval/txt/filename.txt');
+}
+
+btn.addEventListener("click",function(e){
+    //輸入的值為 keyword.value
+    let num=0;
+    let weight=[];
+
+    
+    const word = keyword.value;
+    axios.all([getWord1475(), getWeight1(), getFilename()])
+    .then(axios.spread(function (getWord, getWei, getFile) {
+        // getWord讀第幾個關鍵字
+        let ary1 = "";
+        ary1 = getWord.data;
+        var strAry1 = ary1.split('\n');
+        
+        strAry1.forEach(function(item,index){
+            if(word === item){
+                num += index;              
+            }
+        })
+
+        // getWei讀權重
+        let ary2 = "";
+        ary2 = getWei.data;
+        var strAry2 = ary2.split('\n');
+        strAry2.forEach(function(item,index){
+            strAry2[index]=strAry2[index].split(' ');
+        })
+        strAry2.forEach(function(item,index){
+            if((item[num] !=0) && (item[num] != undefined)){
+                let a=[];
+                a.push(parseFloat(item[num]));
+                a.push(index);
+                weight.push(a);
+            }
+        })
+
+        // getFile
+        let ary3 = "";
+        ary3 = getFile.data;
+        var strAry3 = ary3.split('\n');
+        weight.forEach(function(item,index){
+            item.push(strAry3[item[1]])
+        })
+
+        // 排序
+        weight.sort(function(x, y){
+            return y[0] - x[0];
+        });
+
+        init(weight);
+        console.log(weight)
+        // console.log(parseInt("12"))
+
+    }));
+    
+})
